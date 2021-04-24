1e3c030d2b32b24ba0b29808aba26381aca2e9a9
coin <kokana@coindeMBP.bbrouter>
2021-04-24 21:36:02 +0800

    ff

--- demo/demo1.py
+++ demo/demo1.py
@@ -10,5 +10,5 @@ data = [data[i:i + 5] for i in range(0, len(data), 5)]
 
 with open("output.csv", "w") as f:
     writer = csv.writer(f)
-    
+    print('helloworld')    
     writer.writerows(data)
